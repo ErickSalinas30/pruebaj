dba22d1bb73bcd4c758035069a1f328c7d8bdba3
usuario <usuario@172.16.223.135>
2022-10-28 10:34:07 -0500

    Actividad_seleccion

--- src/main/java/com/mycompany/ejercicio_01_03/Ejercicio_01_03.java
+++ src/main/java/com/mycompany/ejercicio_01_03/Ejercicio_01_03.java
@@ -31,11 +31,22 @@ public class Ejercicio_01_03 {
         var lobo=new Pelicula(12000000,25,"Lobo Walls Street","Martin Scorsese",dicaprio);
         lobo.setActorFamoso(dicaprio);
         
+        System.out.println("La pelicula de nombre "+lobo.getTitulo()+"realizada con un presupuesto de "
+                +lobo.getPresupuesto()+" con el famoso actor"+lobo.getActorFamoso().getNombre()
+                +" y producida por: "+lobo.getnProductor());
+        
         var inter=new Pelicula(25000000,12,"Interstellar","Christopher Nolan",matthew);
         inter.setActorFamoso(matthew);
         
+        System.out.println("La pelicula de nombre "+inter.getTitulo()+"realizada con un presupuesto de "
+                +inter.getPresupuesto()+" con el famoso actor"+inter.getActorFamoso().getNombre()
+                +" y producida por: "+inter.getnProductor());
+        
         System.out.println(dicaprio.mostrarInfo());
         System.out.println(matthew.mostrarInfo());
+        System.out.println(jack.mostrarInfo());
+        System.out.println(josep.mostrarInfo());
+        
         
         
         
